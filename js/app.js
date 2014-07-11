b4be97b99f946ec912e999f413c38f467dd74402
tecnoparanoids <tecnoparanoids@radioela.org>
2014-07-11 19:50:26 +0200

    a?adido play/stop en los episodios del pocast

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var rssLoaded = false;
 
 $(document).ready(function(){
-	var podcast = new PodcastPlayer("player_hq");
 });
 
 function parseRSS(url, container) {
@@ -17,40 +16,38 @@ function parseRSS(url, container) {
       function(i,e){
       	console.log("Audio: " + $(e).find("enclosure").attr('url'));
       	var color = ""
-      	var thehtml = '<li><div class="link"><a href="'+ $(e).find("enclosure").attr('url') +'" target="_blank">'+
-      					$(e).find("title").text()+'</a></div></li>';
+      	var thehtml = '<li><div class="link" value="' + $(e).find("enclosure").attr('url') + '">' +
+      					$(e).find("title").text()+'</div></li>';
 //		console.log("Agregado capítulo: " + thehtml);        
         $(container).append(thehtml);
         rssLoaded = true;
+        
+        $("#episodes li").click(function () {
+            playShow(this);
+        });
       }
       );
-      
-//      console.log($(xmlDoc).find("enclosure").attr("url"));
-//	var url =  $(xml.items[0]).find("enclosure").attr('url');
-
-//      $(container).html('<h2>'+ data.responseData.feed.title +'</h2>');
-//      console.log("Agregado título: " + data.responseData.feed);
     }
   });
 };
 
 var switchTab = function( idTab, idTabHide ){
 
+    console.log("switch tab!");
     document.getElementById(idTab).style.zIndex='1';
     document.getElementById(idTabHide).style.zIndex='2';
-    console.log("switch tab!");
     
     if (!rssLoaded)
 		parseRSS("http://radioela.org/spip.php?page=backend&id_rubrique=5","#episodes");
 
-    console.log("salgo de switchTab");	
+//    console.log("salgo de switchTab");	
 }
 
 var playStreaming = function(player){
 
 	var audio = document.getElementById(player);
 	if(audio.paused){
-		console.log("PLAY!!!!" + player + audio.paused);
+		console.log("PLAY!!!!" + player);
 		audio.play();
 	}
 	else{
@@ -58,4 +55,18 @@ var playStreaming = function(player){
 		audio.pause();
 	}
 		
+}
+
+var playShow = function (link){
+	console.log("Programa clickado: " + $($(link).html()).attr('value'));
+	var audio = document.getElementById('podcast_player');
+	audio.src = $($(link).html()).attr('value');
+	if(audio.paused){
+		console.log("PLAY!!!!" + audio.src);
+		audio.play();
+	}
+	else{
+		console.log("STOP!!!!" + audio.src);
+		audio.pause();
+	}
 }
